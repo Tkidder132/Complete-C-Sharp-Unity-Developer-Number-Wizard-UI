f3ecb808a00c20f7ac38b1aa5b9f933cdad0a759
Tristan <tkidder132@gmail.com>
2017-10-15 03:06:17 -0500

    Title Screen text and buttons added

---
+++ Assets/_Scripts/LevelManagerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class LevelManagerController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for " + name);
+    }
+}
